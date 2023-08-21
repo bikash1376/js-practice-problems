203288529674657e0d44ce7cc7228af0fe4ae1a1
bikash1376 <139378098+bikash1376@users.noreply.github.com>
2023-08-21 23:52:18 +0530

    Create basicCalculations.js

---
+++ basicCalculations.js
@@ -0,0 +1,7 @@
+let a = 50;
+let b = 24;
+console.log(a+b);     //addition or sum
+console.log(a-b);     //subtraction or minus
+console.log(a*b);     //multiplication or product
+console.log(a/b);    //division i.e. quotient
+console.log(a%b);    //modulus or remainder
